fa6cbae104268dfde2d9745bfe75bfbe0b58ecea
Ean Jason <ecvelayo@gmail.com>
2020-07-26 19:36:32 +0800

    Updates as of July 26, 2020

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,7 +5,8 @@
 $route = env('PACKAGE_ROUTE', '').'/ledger/';
 $controller = 'Increment\Finance\Http\LedgerController@';
 Route::post($route.'create', $controller."create");
-Route::post($route.'retrieve', $controller."retrieve");
+Route::post($route.'retrieve_by_id', $controller."retrieveByID");
+Route::post($route.'retrieve', $controller."retrieveByCode");
 Route::post($route.'retrieve_all', $controller."retrieveAll");
 Route::post($route.'update', $controller."update");
 Route::post($route.'delete', $controller."delete");
