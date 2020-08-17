be6a564ece3bdb5f1ff8b7c5662cdb56773f75e5
kennCK <kennettecanales@gmail.com>
2020-08-17 15:11:53 +0800

    update

--- src/routes/web.php
+++ src/routes/web.php
@@ -36,6 +36,3 @@ $route = env('PACKAGE_ROUTE', '').'/withdrawals/';
 $controller = 'Increment\Finance\Http\WithdrawalController@';
 Route::post($route.'create', $controller."create");
 Route::post($route.'retrieve', $controller."retrieve");
-Route::post($route.'update', $controller."update");
-Route::post($route.'delete', $controller."delete");
-Route::get($route.'test', $controller."test");
